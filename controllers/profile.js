0152695a22c6ad087a731ff8963a905b3de4e972
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-29 12:46:56 +0300

    Creating and Updating Profile Routes and Controller and Getting All Profiles & Profile By UserId

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 const Profile = require('../models/Profile');
 const User = require('../models/User');
 
@@ -5,7 +7,7 @@ exports.getCurrentUserProfile = async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.user.id,
-    }).populate('User', ['name', 'avatar']);
+    }).populate('user', ['name', 'avatar']);
 
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
@@ -14,6 +16,91 @@ exports.getCurrentUserProfile = async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err);
+    res.status(500).send('Server Error!');
+  }
+};
+
+exports.postCreateProfile = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    company,
+    location,
+    website,
+    bio,
+    skills,
+    status,
+    githubusername,
+    youtube,
+    twitter,
+    instagram,
+    linkedin,
+    facebook,
+  } = req.body;
+
+  const profileFields = {
+    user: req.user.id,
+    company: company ? company : '',
+    website: website ? website : '',
+    location: location ? location : '',
+    bio: bio ? bio : '',
+    status: status ? status : '',
+    githubusername: githubusername ? githubusername : '',
+    skills,
+    social: {
+      youtube: youtube ? youtube : '',
+      twitter: twitter ? twitter : '',
+      instagram: instagram ? instagram : '',
+      linkedin: linkedin ? linkedin : '',
+      facebook: facebook ? facebook : '',
+    },
+  };
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    // Using upsert option (creates new doc if no match is found):
+    // Update or Create
+    profile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profileFields },
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error!');
+  }
+};
+
+exports.getAllProfiles = async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error!');
+  }
+};
+
+exports.getProfileByUserId = async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found!' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found!' });
+    }
     res.status(500).send('Server Error!');
   }
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check } = require('express-validator');
 
 const profileController = require('../../controllers/profile');
 
@@ -8,5 +9,30 @@ const profileController = require('../../controllers/profile');
 // @desc    Get current user profile
 // @access  Private
 router.get('/me', auth, profileController.getCurrentUserProfile);
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  profileController.postCreateProfile
+);
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', profileController.getAllProfiles);
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', profileController.getProfileByUserId);
 
 module.exports = router;
